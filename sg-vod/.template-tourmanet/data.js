58c24c41ccc08bfeed5a3cead197cbd1fc813994
starcrunsher <starcrunsher@gmail.com>
2019-06-01 20:05:16 +0200

    updated sg-vod templates

--- sg-vod/.template-tourmanet/data.js
+++ sg-vod/.template-tourmanet/data.js
@@ -1,19 +1,28 @@
 let tournamentName = "TOURNAMENT NAME";
 let dataObject = [
   {
-    players: [""], channel: "", twitch_vod: "", youtube_vod: "",
-    notes: ""
+    players: [""], channel: [
+      { name: "", twitch_vod: "", youtube_vod: "", notes: "" }
+    ]
   },
   {
-    players: ["", ""], channel: "", twitch_vod: "", youtube_vod: "",
-    notes: "",
+    players: [""], channel: [
+      { name: "", twitch_vod: "", youtube_vod: "", notes: "" }
+    ],
     isSemiFinals: false, isLosersFinals: false, isFinals: false
   }
 ];
 /*
 ,
 {
-  players: [""], channel: "", twitch_vod: "", youtube_vod: "",
-  notes: ""
+  players: [""], channel: [
+    { name: "", twitch_vod: "", youtube_vod: "", notes: "" }
+  ]
+},
+{
+  players: [""], channel: [
+    { name: "", twitch_vod: "", youtube_vod: "", notes: "" }
+  ],
+  isSemiFinals: false, isLosersFinals: false, isFinals: false
 }
 */
