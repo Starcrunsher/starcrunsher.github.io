3fdba4d23e14f9abc66d791effa4ff528d9ed94b
starcrunsher <starcrunsher@gmail.com>
2019-06-02 12:50:09 +0200

    added sm-dab-tournament-2019 to sg-vod

---
+++ sg-vod/sm-dab-tournament-2019/data.js
@@ -0,0 +1,210 @@
+let tournamentName = "SM DAB Tournament 2019";
+let tournamentDescription =`Super Metroid Double Anti-Bingo Tournament 2019
+Rules for Double Anti-Bingo: http://pastebin.com/3vv8vGtq
+Schedule: http://speedgaming.org/smdab
+Bracket: https://supermetroid.challonge.com/smdab19
+Hosted on https://twitch.tv/speedgaming
+http://speedgaming.org/events
+#super metroid #speedrunnning #speedgaming`;
+let tournamentTags = "sm, super metroid, any, super nintendo, snes, tournament, race, rta, speedrun, speed run, nintendo";
+let dataObject = [
+  {
+    players: ["swap_space vs MassHesteria"], channel: [
+      { name: "SpeedGaming", twitch_vod: "429552889", youtube_vod: "Xf7qpwuiNjw", notes: "" }
+    ]
+  },
+  {
+    players: ["Rumbleminze vs ARC"], channel: [
+      { name: "SpeedGaming6", twitch_vod: "429553699", youtube_vod: "Wfx8Vu0BZf8", notes: "" }
+    ]
+  },
+  {
+    players: ["iora vs Di10"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429554552", youtube_vod: "BVEOdM7a8d4", notes: "" }
+    ]
+  },
+  {
+    players: ["papert1ger vs croakomire"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429555695", youtube_vod: "HM7MF9uypNY", notes: "" }
+    ]
+  },
+  {
+    players: ["polarbearbunny vs Mesyr"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429557123", youtube_vod: "VBXXSr-noNM", notes: "" }
+    ]
+  },
+  {
+    players: ["TracieM vs Janus"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429558007", youtube_vod: "R2yHtHThtzc", notes: "" }
+    ]
+  },
+  {
+    players: ["sloaters27 vs Aerytheta"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "429559284", youtube_vod: "wsSplulRTok", notes: "" }
+    ]
+  },
+  {
+    players: ["CanadianOwl vs MrGuyAverage"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429560415", youtube_vod: "4AYK5tQzXDg", notes: "" }
+    ]
+  },
+  {
+    players: ["sweetnumb vs FusionWarrior"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429561285", youtube_vod: "RufM_z2aw90", notes: "" }
+    ]
+  },
+  {
+    players: ["Feral vs Toasty"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429562653", youtube_vod: "oaRNmxVMa3I", notes: "" }
+    ]
+  },
+  {
+    players: ["Ddraig vs crossproduct"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429563871", youtube_vod: "7HWW2W9t9cQ", notes: "" }
+    ]
+  },
+  {
+    players: ["BluePlasmaSpark vs FreyasSpirit"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429564985", youtube_vod: "zneNmGq_4J4", notes: "" }
+    ]
+  },
+  {
+    players: ["CheeseReaver vs LexausaurusRex"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429565800", youtube_vod: "5iWevO26WyY", notes: "" }
+    ]
+  },
+  {
+    players: ["Belthasar vs iora"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429566514", youtube_vod: "_WEP2JxZCsc", notes: "" }
+    ]
+  },
+  {
+    players: ["azder vs NightwolfXVI"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "429567979", youtube_vod: "pMpc9W-__c0", notes: "End of post-game interview was cut off" }
+    ]
+  },
+  {
+    players: ["Whewty vs croakomire"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "429570089", youtube_vod: "YVf1C2itlDc", notes: "" }
+    ]
+  },
+  {
+    players: ["Kipp vs TracieM"], channel: [
+      { name: "SpeedGaming5", twitch_vod: "429571527", youtube_vod: "2x4qPBouOzo", notes: "" }
+    ]
+  },
+  {
+    players: ["sweetnumb vs Maisgueuh"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "429572592", youtube_vod: "ZlfbEEaPi-M", notes: "" }
+    ]
+  },
+  {
+    players: ["iora vs swap_space"], channel: [
+      { name: "SpeedGaming6", twitch_vod: "429988462", youtube_vod: "8QrXuLEBBv4", notes: "" }
+    ]
+  },
+  {
+    players: ["PeteZa_34 vs polarbearbunny"], channel: [
+      { name: "SpeedGaming", twitch_vod: "429990321", youtube_vod: "4PN87d-X2oU", notes: "" }
+    ]
+  },
+  {
+    players: ["64bit_link vs CanadianOwl"], channel: [
+      { name: "SpeedGaming", twitch_vod: "429993537", youtube_vod: "H5FWjRwJoao", notes: "" }
+    ]
+  },
+  {
+    players: ["azder vs Hero_82"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "430444347", youtube_vod: "an7wmVEN8_g", notes: "" }
+    ]
+  },
+  {
+    players: ["serberoth vs crossproduct"], channel: [
+      { name: "SpeedGaming", twitch_vod: "430446528", youtube_vod: "dceN4PfFtl4", notes: "" }
+    ]
+  },
+  {
+    players: ["Gameaholic vs Feral"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "430449263", youtube_vod: "iE0yZMfTlzQ", notes: "" }
+    ]
+  },
+  {
+    players: ["MassHesteria vs SpaghettiToastBook"], channel: [
+      { name: "SpeedGaming5", twitch_vod: "430452092", youtube_vod: "qpIKbW-otgk", notes: "" }
+    ]
+  },
+  {
+    players: ["Rumbleminze vs Jaychalke"], channel: [
+      { name: "SpeedGaming", twitch_vod: "430919387", youtube_vod: "qDZYswup8vo", notes: "" }
+    ]
+  },
+  {
+    players: ["WhiteMageBecky vs Skarsnik"], channel: [
+      { name: "SpeedGaming", twitch_vod: "430921756", youtube_vod: "XffX8EKIZAg", notes: "" }
+    ]
+  },
+  {
+    players: ["Kipp vs polarbearbunny"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "430923558", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["ptoil vs derp"], channel: [
+      { name: "SpeedGaming", twitch_vod: "432973027", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["azder vs sweetnumb"], channel: [
+      { name: "SpeedGaming2", twitch_vod: "432973936", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["mrguyaverage (guyaverage vs Maisgueuh"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "432975257", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["EasyDanger vs LexausaurusRex"], channel: [
+      { name: "SpeedGaming6", twitch_vod: "432976923", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["PeteZa_34 vs Toasty"], channel: [
+      { name: "SpeedGaming6", twitch_vod: "432977535", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["Gameaholic vs Mesyr"], channel: [
+      { name: "SpeedGaming4", twitch_vod: "432978378", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["sloaters27 vs BFrobber"], channel: [
+      { name: "SpeedGaming5", twitch_vod: "432979173", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["64bit_link vs FusionWarrior"], channel: [
+      { name: "SpeedGaming3", twitch_vod: "433401965", youtube_vod: "", notes: "" }
+    ]
+  },
+  {
+    players: ["SpaghettiToastBook vs Di10"], channel: [
+      { name: "SpeedGaming4", twitch_vod: "433404045", youtube_vod: "", notes: "" }
+    ]
+  }
+];
+/*
+,
+{
+  players: [""], channel: [
+    { name: "", twitch_vod: "", youtube_vod: "", notes: "" }
+  ]
+},
+{
+  players: [""], channel: [
+    { name: "", twitch_vod: "", youtube_vod: "", notes: "" }
+  ],
+  isSemiFinals: false, isLosersFinals: false, isFinals: false
+}
+*/
